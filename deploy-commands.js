150a2dc9dc030051ae9ebcfa4054ad67ca0429d2
TalonDragon000 <talon_dragon000@hotmail.com>
2021-08-28 21:13:47 -1000

    updated deploy-commands with ID info to setup slash commadns

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,23 +6,30 @@ const { clientId, guildId, token } = require('./config.json');
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+const clientId = "876692485508853801";
+const guildId = "876226119018377267";
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
+/* Saving this in case I might need it later.
 !map(command => command.toJSON());
+*/
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
 	try {
+		console.log('Started refreshing application (/) commands.');
+
 		await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
-
-		console.log('Successfully registered application commands.');
+		
+		console.log('Successfully reloaded application (/) commands.');
 	} catch (error) {
 		console.error(error);
 	}
