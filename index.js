d43297b8c9c00267631b2c4074e620e3a620ca5d
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2021-08-29 01:52:38 -1000

    prefix
    added a prefix code

--- index.js
+++ index.js
@@ -34,6 +34,19 @@ client.on("message", (message) => {
   }
 });
 
+const prefix = "!";
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(prefix)) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
+  }
+});
+
 client.on(`interactionCreate`, async interaction => {
     if (!interaction.isCommand()) return;
 
